a9c2e4fcc10b80dbd8c47ccb35f344cb36cde40a
oltree <o.meleh10@gmail.com>
2022-08-02 14:33:44 +0300

    update project

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 
 import { countersManagerReducer } from "../pages/ReduxCounters/reducers";
-//import { tasksManagerReducer } from "../pages/TodoList/reducers";
 
 export const rootReducer = combineReducers({
   countersManager: countersManagerReducer,
-  //tasksManager: tasksManagerReducer,
 });
